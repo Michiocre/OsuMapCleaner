27c60fd7f5bff72404d071010c8c7c74f27a7611
Michael Blank <michi2806@gmail.com>
2017-12-02 05:22:48 +0100

    Commenting is so boring

--- OsuMapCleaner/OsuMapCleaner/Program.cs
+++ OsuMapCleaner/OsuMapCleaner/Program.cs
@@ -12,78 +12,96 @@ namespace OsuMapCleaner
 {
     class Program
     {
-        static int x = 0;
+        static int FileCount = 0;
         static long s = 0;
 
+        //All the Extensions Counting as images
         static List<string> imageList = new List<string>(new string[] {".png", ".jpg", ".jpeg"});
 
+        //All the files that count as music files who will be whitelisted
         static List<string> musicWhiteList = new List<string>(new string[] { ".mp3", ".ogg"});
 
+        //All the other files that will be whitelisted
         static List<string> whiteList = new List<string>(new string[] {".osu", ".osz", ".osr"});
-        
-        static List<string> backgrounds;
 
+        //Everything that is whitelisted will not get deleted no matter what
+
+        //Initialisation of some generaly used Lists
         static List<string> numbers = new List<string>();
+        static List<string> noBackgroundList = new List<string>();
 
         static void Main(string[] args)
         {
             whiteList.AddRange(musicWhiteList);
 
-            string[] folders = Directory.EnumerateDirectories(Directory.GetCurrentDirectory()).ToArray<string>();
-            string input = Console.ReadLine();
+            string[] beatmapFolders = Directory.EnumerateDirectories(Directory.GetCurrentDirectory()).ToArray<string>();
+            string input = Console.ReadLine(); //Waiting for the first enter
+            
 
-            List<string> noBackgroundFolder = new List<string>();
+            //Main Part of the Code here the files will either get deleted or let through
 
-
-            //Main Part
-
+            //Variables used to calculate how far through the folders we are
             double lastPercent = -1;
+            double percent;
 
-            for (int i = 0; i < folders.Length; i++)
+            for (int i = 0; i < beatmapFolders.Length; i++)
             {
-                string folder = folders[i];
-                double percent = Math.Round(i / (folders.Length / 100.0));
-                List<string> files = Directory.EnumerateFiles(folder).ToList<string>();
+                string currentFolder = beatmapFolders[i];
+                percent = Math.Round(i / (beatmapFolders.Length / 100.0));
+                List<string> files = Directory.EnumerateFiles(currentFolder).ToList<string>();
 
-                backgrounds = new List<string>();
-
-                //Searching for the Backgrounds     
+                //Searching for the Backgrounds   
+                List<string> backgrounds = new List<string>();
                 string noBackground = "";
+                
+                //During the first iteration it will read all the .osu files and search for all the used background files
                 foreach (string file in files)
                 {
-                    string extension = Path.GetExtension(file).ToLower();
-
-                    if (extension == ".osu")
+                    if (Path.GetExtension(file).ToLower() == ".osu")
                     {
                         string raw = File.ReadAllText(file);
                         try
                         {
-                            string bgName = Regex.Split(Regex.Split(raw, "0,0,\"")[1], "\"")[0];
+                            string bgName = Regex.Split(Regex.Split(raw, "0,0,\"")[1], "\"")[0]; //The backgroundname should be between the first two quotes
 
-                            if (imageList.Contains(Path.GetExtension(bgName).ToLower()))
+                            if (imageList.Contains(Path.GetExtension(bgName).ToLower())) //If it isnt an image it will try again between the next two quotes
                             {
-                                backgrounds.Add(bgName);
+                                if (!backgrounds.Contains(bgName))
+                                {
+                                    backgrounds.Add(bgName);
+                                }
+                            }
+                            else
+                            {
+                                bgName = Regex.Split(Regex.Split(raw, "0,0,\"")[3], "\"")[2];
+                                if (imageList.Contains(Path.GetExtension(bgName).ToLower())) //If it still isnt an image there is no background
+                                {
+                                    if (!backgrounds.Contains(bgName))
+                                    {
+                                        backgrounds.Add(bgName);
+                                    }
+                                }
                             }
                         }
-                        catch (IndexOutOfRangeException)
+                        catch (IndexOutOfRangeException) //If there are no quotations in the whole file there cant be a background -> these will get written in the noBackground.txt
                         {
-                            noBackground = folder;
+                            noBackground = currentFolder;
                         }
                     }
                 }
 
                 if (noBackground != "")
                 {
-                    noBackgroundFolder.Add(noBackground);
+                    noBackgroundList.Add(noBackground);
                 }
 
-                //Done With Searching
+                //Now we have all the background filenames, and now which files dont have a background
 
                 foreach (string file in files)
                 {
                     bool delete = true;
-                    string extension = Path.GetExtension(file).ToLower();
-                    if (whiteList.Contains(extension))
+                    string extension = Path.GetExtension(file).ToLower(); //This is very important as sometimes there will be: mp3, MP3, Mp3
+                    if (whiteList.Contains(extension)) //If its whitelisted
                     {
                         delete = false;
                     }
@@ -91,7 +109,7 @@ namespace OsuMapCleaner
                     {
                         foreach (string bg in backgrounds)
                         {
-                            if (Path.GetFileName(file) == bg || Path.GetFileName(file).ToLower() == bg.ToLower())
+                            if (Path.GetFileName(file) == bg || Path.GetFileName(file).ToLower() == bg.ToLower()) //Sometimes in the file its BG.png, but the file is called bg.png
                             {
                                 delete = false;
                             }
@@ -104,57 +122,52 @@ namespace OsuMapCleaner
                     }
                 }
 
-                foreach (string dir in Directory.EnumerateDirectories(folder))
+                foreach (string dir in Directory.EnumerateDirectories(currentFolder)) //Deletes all the folders inside the Beatmap folder -> Mostly SB (Storyboard folder)
                 {
-                    DeleteFolder(dir);
+                    DeleteFolder(dir, backgrounds);
                 }
 
-                if (percent > lastPercent)
+                if (percent > lastPercent) //If we have gone up by one percent it will print again
                 {
                     Console.WriteLine("Percent: " + percent + "%");
                     lastPercent = percent;
                 }
             }
 
-            Console.WriteLine(x + " Files were deleted");
+            //s is the amount of storage cleard in bytes
+
+            Console.WriteLine(FileCount + " Files were deleted");
             if (s >= 1000000000.0)
             {
-                Console.WriteLine(s / 1000000000.0 + " GB were saved");
+                Console.WriteLine(Math.Round(s / 1000000000.0, 2) + " GB were saved"); //If its more then 1 gb it will say   x,xx GB
             }
             else if (s >= 1000000.0)
             {
-                Console.WriteLine(s / 1000000.0 + " MB were saved");
+                Console.WriteLine(Math.Round(s / 1000000.0, 2) + " MB were saved"); //If its more then 1 mb it will say   x,xx MB
             }
             else if (s >= 1000.0)
             {
-                Console.WriteLine(s / 1000.0 + " KB were saved");
+                Console.WriteLine(Math.Round(s / 1000.0, 2) + " KB were saved"); //If its more then 1 kb it will say   x,xx KB
             }
             else
             {
-                Console.WriteLine(s + " B were saved");
+                Console.WriteLine(s + " B were saved"); //Else its    x B
             }
 
-            File.WriteAllLines(Directory.GetCurrentDirectory() + "\\noBackground.txt", noBackgroundFolder);
 
             Console.WriteLine("#-----------------------#");
             Console.WriteLine("All the Files were deleted, now its searching for Errors (this will be much quicker)");
             Console.WriteLine("#-----------------------#");
 
             //Finding out if all the maps have at least one background (Exept when thats intended)
-            string noBgPath = Directory.GetCurrentDirectory() + "\\noBackground.txt";
-            List<string> noBackgrounds = new List<string>();
-            if (File.Exists(noBgPath))
+            foreach (string folder in beatmapFolders)
             {
-                noBackgrounds.AddRange(File.ReadAllLines(noBgPath).ToList<string>());
-            }
-            foreach (string folder in folders)
-            {
-                int x = 0;
-                if (noBackgrounds.Count > 0)
+                bool hasBackground = false;
+                if (noBackgroundList.Count > 0)
                 {
-                    if (noBackgrounds.Contains(folder))
+                    if (noBackgroundList.Contains(folder)) //If the beatmap doesnt have a background (as read from the .osu file)
                     {
-                        x = 1;
+                        hasBackground = true;
                     }
                 }
 
@@ -172,16 +185,16 @@ namespace OsuMapCleaner
                     {
                         if (imageList.Contains(Path.GetExtension(file).ToLower()))
                         {
-                            x++;
+                            hasBackground = true;
                         }
                     }
                 }
 
-                if (x == 0)
+                if (!hasBackground)
                 {
                     string outp = folder.Split('\\')[folder.Split('\\').Length - 1];
 
-                    numbers.Add(outp.Split(' ')[0]);
+                    numbers.Add(outp.Split(' ')[0]);    //Lists all the map ids
 
                     Console.WriteLine("Check: " + outp + "   | for missing images");
                 }
@@ -190,37 +203,40 @@ namespace OsuMapCleaner
             Console.WriteLine("First iteration complete");
             Console.WriteLine("#-----------------------#");
 
-            foreach (string folder in folders)
+
+            //Finding out if all the maps have at least one music file
+            foreach (string folder in beatmapFolders)
             {
                 List<string> files = Directory.EnumerateFiles(folder).ToList<string>();
-                int x = 0;
+                bool hasMusic = false;
                 foreach (string file in files)
                 {
                     if (musicWhiteList.Contains(Path.GetExtension(file).ToLower()))
                     {
-                        x++;
+                        hasMusic = true;
                     }
                 }
-                if (x == 0)
+                if (!hasMusic)
                 {
                     string outp = folder.Split('\\')[folder.Split('\\').Length - 1];
                     string number = outp.Split(' ')[0];
                     if (!numbers.Contains(number))
                     {
-                        numbers.Add(number);
+                        numbers.Add(number);        //Lists the beatmap ids when its missing
                     }
 
                     Console.WriteLine("Check: " + outp + "   | for missing Audio");
                 }
             }
 
+            //Creates the Text for the error file
             string fileWrite = "";
 
             foreach (string number in numbers)
             {
                 try
                 {
-                    Convert.ToInt32(number);
+                    Convert.ToInt32(number); // This sorts out all the Beatmaps without ids
                     fileWrite += number + ",";
                 }
                 catch (Exception)
@@ -229,7 +245,7 @@ namespace OsuMapCleaner
                 }
             }
 
-            fileWrite = fileWrite.Trim(',');
+            fileWrite = fileWrite.Trim(','); //Deltes trailing ,
 
             File.WriteAllText(Directory.GetCurrentDirectory() + "\\ErrorList.txt", fileWrite);
             Console.WriteLine("#-----------------------#");
@@ -238,46 +254,44 @@ namespace OsuMapCleaner
             Console.ReadLine();
         }
 
+        //Deletes a File and adds its size to the static s
         static public void DeleteFile(string file)
         {
             s += new FileInfo(file).Length;
             File.Delete(file);
             Console.WriteLine(file);
-            x++;
+            FileCount++;
         }
 
-        static public void DeleteFolder(string folder) {
+        //Function deletes a whole folder exept if there is a background in it -> then it will delte everything exept the background file
+        static public void DeleteFolder(string folder, List<string> backgrounds) {
             foreach (string file in Directory.EnumerateFiles(folder))
             {
-                int why = 0;
+                bool isBackground = false;
                 foreach (string bg in backgrounds)
                 {
-                    if (Path.GetFileName(bg) == Path.GetFileName(file))
+                    if (Path.GetFileName(bg) == Path.GetFileName(file) || Path.GetFileName(bg) == Path.GetFileName(file).ToLower())
                     {
-                        why++;
+                        isBackground = true;
                     }
                 }
 
-                if (why == 0)
+                if (!isBackground)
                 {
-
                     DeleteFile(file);
-                }
-                else
-                {
-                    
                 }
             }
             foreach (string dir in Directory.EnumerateDirectories(folder))
             {
-                DeleteFolder(dir);
+                DeleteFolder(dir, backgrounds);
             }
-            if (Directory.EnumerateFiles(folder).Count() + Directory.EnumerateDirectories(folder).Count() == 0)
+            if (Directory.EnumerateFiles(folder).Count() + Directory.EnumerateDirectories(folder).Count() == 0) // After deleting everything exept background, if there are files left in the folder it gets spared
             {
                 Directory.Delete(folder);
             }
         }
 
+        //Function to get all subfolders of all subfolder of all subfolder ...
         static public List<string> getSubFolders(string folder, List<string> addHere)
         {
             foreach (string subFolder in Directory.EnumerateDirectories(folder))
--- OsuMapCleaner/OsuMapCleaner/script.ahk
+++ OsuMapCleaner/OsuMapCleaner/script.ahk
@@ -1,15 +1,15 @@
 ﻿^j::
 	FileRead, Content, %A_WorkingDir%\ErrorList.txt
-	StringSplit, array, Content, `,
+	StringSplit, array, Content, `, ;Splits at ','
 	array := Trim(array)
-	Send, ^n
-	Sleep, 100
+	Send, ^n ;Firstly opens a new browserwindow
+	Sleep, 100 ;Waiting for the window
 	Loop, %array0%
 	{
 		this_number := array%a_index%
-		Send, https://osu.ppy.sh/d/%this_number%n
+		Send, https://osu.ppy.sh/d/%this_number%n ;Types the direct downloadling (the trailing n is for no Video)
 		Send, {Enter}
-		Send, ^t
+		Send, ^t ;Open new Tab
 	}
-	Send, ^w
+	Send, ^w ;Closes the last tab
 Return
